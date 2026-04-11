13efb24d49d6ae0b69189e6e7937d1897b474625
Codewith-Hein <codewithhein2@gmail.com>
2026-04-11 22:21:12 +0630

    add post feature pagination and gilter

--- src/main/java/com/lostandfound/app/repository/PostRepository.java
+++ src/main/java/com/lostandfound/app/repository/PostRepository.java
@@ -5,16 +5,10 @@ import com.lostandfound.app.model.PostType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface PostRepository extends JpaRepository<Post,Long> {
-    @Query(
-            value = "SELECT p FROM Post p " +
-                    "JOIN FETCH p.user " +
-                    "JOIN FETCH p.category " + // Add this line!
-                    "WHERE p.postType = :type",
-            countQuery = "SELECT count(p) FROM Post p WHERE p.postType = :type"
-    )
-    Page<Post> findByPostTypeWithUserAndCategory(@Param("type") PostType type, Pageable pageable);
+public interface PostRepository extends JpaRepository<Post,Long> , JpaSpecificationExecutor<Post> {
+
 }
